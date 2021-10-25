98fd2b3cfdbf1750e3b0b5c72cc98fdbbed61e17
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-25 17:30:47 -0400

    add method signatures

--- src/main/java/com/nttdata/foodorderingapp/dao/FoodDAO.java
+++ src/main/java/com/nttdata/foodorderingapp/dao/FoodDAO.java
@@ -1,5 +1,18 @@
 package com.nttdata.foodorderingapp.dao;
 
-public interface FoodDAO {
+import java.util.List;
 
+import com.nttdata.foodorderingapp.model.Dish;
+import com.nttdata.foodorderingapp.model.Order;
+
+public interface FoodDAO {
+	public String login(String user, String pass);
+	public List<Dish> getAllDishes();
+	public void addDishToMenu(Dish dish);
+	public void addDishToDishes(Dish dish);
+	public void deleteDishFromMenu(Dish dish);
+	public int addOrderToOrders(Order order);
+	public void addDishesToOrderDetails(List<Order> orders);
+	public List<Order> getOrdersWithUser(int userId);
+	public List<Order> searchOrders(int userId, String partialOrderId);
 }
