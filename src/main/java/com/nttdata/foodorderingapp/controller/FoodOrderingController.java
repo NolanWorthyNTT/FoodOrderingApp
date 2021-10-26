aa771dc4ab1c89312350529818630680c35388ad
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-26 12:20:36 -0400

    enable CORS. fix login to respond with JSON object.

--- src/main/java/com/nttdata/foodorderingapp/controller/FoodOrderingController.java
+++ src/main/java/com/nttdata/foodorderingapp/controller/FoodOrderingController.java
@@ -1,8 +1,10 @@
 package com.nttdata.foodorderingapp.controller;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,14 +14,16 @@ import com.nttdata.foodorderingapp.dao.FoodDAO;
 import com.nttdata.foodorderingapp.dao.FoodDAOImpl;
 import com.nttdata.foodorderingapp.model.Dish;
 
+@CrossOrigin
 @RestController
 public class FoodOrderingController {
 	
 	FoodDAO foodDAO = new FoodDAOImpl();
 	
 	@PostMapping("/login")
-	public String login(@RequestBody Map<String, String> json) {
-		return foodDAO.login(json.get("username"), json.get("password"));
+	public Map<String, String> login(@RequestBody Map<String, String> json) {
+		String response = foodDAO.login(json.get("username"), json.get("password"));
+		return Collections.singletonMap("response", response);
 	}
 	
 	@GetMapping("/menu")
