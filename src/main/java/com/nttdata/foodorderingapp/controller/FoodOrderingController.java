e7f82ca32b0ccd18cfba0f13d6d8a8ff202a5fde
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-26 10:21:31 -0400

    add endpoint for getAllDishesFromMenu

--- src/main/java/com/nttdata/foodorderingapp/controller/FoodOrderingController.java
+++ src/main/java/com/nttdata/foodorderingapp/controller/FoodOrderingController.java
@@ -1,13 +1,16 @@
 package com.nttdata.foodorderingapp.controller;
 
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nttdata.foodorderingapp.dao.FoodDAO;
 import com.nttdata.foodorderingapp.dao.FoodDAOImpl;
+import com.nttdata.foodorderingapp.model.Dish;
 
 @RestController
 public class FoodOrderingController {
@@ -17,5 +20,10 @@ public class FoodOrderingController {
 	@PostMapping("/login")
 	public String login(@RequestBody Map<String, String> json) {
 		return foodDAO.login(json.get("username"), json.get("password"));
+	}
+	
+	@GetMapping("/menu")
+	public List<Dish> getAllDishesOnMenu() {
+		return foodDAO.getAllDishesOnMenu();
 	}
 }
