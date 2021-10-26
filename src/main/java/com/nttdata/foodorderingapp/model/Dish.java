e0e4842ba876dad4a7ecb9b25428af63eff2fbd8
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-26 10:13:36 -0400

    add qtyAvailable member to Dish model

--- src/main/java/com/nttdata/foodorderingapp/model/Dish.java
+++ src/main/java/com/nttdata/foodorderingapp/model/Dish.java
@@ -2,13 +2,15 @@ package com.nttdata.foodorderingapp.model;
 
 public class Dish {
 	private String dishName;
+	private int qtyAvailable;
 	private String pricePer;
 	private String imageUrl;
 	private String ingredients;
 	
-	public Dish(String dishName, String pricePer, String imageUrl, String ingredients) {
+	public Dish(String dishName, int qtyAvailable, String pricePer, String imageUrl, String ingredients) {
 		super();
 		this.dishName = dishName;
+		this.qtyAvailable = qtyAvailable;
 		this.pricePer = pricePer;
 		this.imageUrl = imageUrl;
 		this.ingredients = ingredients;
@@ -20,6 +22,14 @@ public class Dish {
 
 	public void setDishName(String dishName) {
 		this.dishName = dishName;
+	}
+	
+	public int getQtyAvailable() {
+		return qtyAvailable;
+	}
+	
+	public void setQtyAvailable(int qtyAvailable) {
+		this.qtyAvailable = qtyAvailable;
 	}
 
 	public String getPricePer() {
