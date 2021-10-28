35c6533edb77e8155bc2fe7bd4bd3a08e1f20c21
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-28 14:42:28 -0400

    add getDetailsWithOrder method signature

--- src/main/java/com/nttdata/foodorderingapp/dao/FoodDAO.java
+++ src/main/java/com/nttdata/foodorderingapp/dao/FoodDAO.java
@@ -3,6 +3,7 @@ package com.nttdata.foodorderingapp.dao;
 import java.util.List;
 
 import com.nttdata.foodorderingapp.model.Dish;
+import com.nttdata.foodorderingapp.model.DishDetails;
 import com.nttdata.foodorderingapp.model.OrderFromTable;
 import com.nttdata.foodorderingapp.model.OrderToInsert;
 import com.nttdata.foodorderingapp.model.User;
@@ -17,5 +18,6 @@ public interface FoodDAO {
 	public int[] addDishToOrderDetails(Dish dish, int orderId);
 	public int reduceQtyOfDishAvailable(Dish dish);
 	public List<OrderFromTable> getOrdersWithUser(int userId);
+	public List<DishDetails> getDetailsWithOrder(int orderId);
 	public List<OrderToInsert> searchOrders(int userId, String partialOrderId);
 }
