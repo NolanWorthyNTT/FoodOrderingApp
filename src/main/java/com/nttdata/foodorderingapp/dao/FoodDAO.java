44f05f83f7fcd3cea37a459d93c25c5f730491b1
NolanWorthyNTT <Nolan.Worthy@nttdata.com>
2021-10-28 21:55:13 -0400

    delete deleteDishFromMenu and searchOrders method signatures. add clearMenu signature.

--- src/main/java/com/nttdata/foodorderingapp/dao/FoodDAO.java
+++ src/main/java/com/nttdata/foodorderingapp/dao/FoodDAO.java
@@ -13,11 +13,10 @@ public interface FoodDAO {
 	public List<Dish> getAllDishesOnMenu();
 	public int addDishToMenu(Dish dish);
 	public int[] addDishToDishes(Dish dish);
-	public void deleteDishFromMenu(Dish dish);
+	public int clearMenu();
 	public int[] addOrderToOrders(OrderToInsert order);
 	public int[] addDishToOrderDetails(Dish dish, int orderId);
 	public int reduceQtyOfDishAvailable(Dish dish);
 	public List<OrderFromTable> getOrdersWithUser(int userId);
 	public List<DishDetails> getDetailsWithOrder(int orderId);
-	public List<OrderToInsert> searchOrders(int userId, String partialOrderId);
 }
